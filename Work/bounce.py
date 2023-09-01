af3462acecfb40789d4cab5874ad8970ba12c99b
johnyb115 <jan.brodsky115@gmail.com>
2023-09-01 16:45:08 +0200

    finished numbers

--- Work/bounce.py
+++ Work/bounce.py
@@ -3,3 +3,10 @@ Exercise 1.5.
 
 bounce.py
 """
+initial_height = 100    # Meters
+n_bounces = 10
+act_height = 0
+for bounce in range(n_bounces):
+    act_height = initial_height*(3/5)
+    initial_height = act_height
+    print(f'{bounce+1} {round(act_height, 4)}')
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -3,3 +3,29 @@ Exercise 1.7.
 
 mortgage.py
 """
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+extra_payment = payment + 1000
+total_paid = 0.0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+count = 1
+while principal > 0:
+    if(principal < payment):
+        principal = principal * (1+rate/12) - (principal)
+        total_paid = total_paid + (payment-principal)
+    else:
+        if(count >= extra_payment_start_month and count <= extra_payment_end_month):
+            principal = principal * (1+rate/12) - extra_payment
+            total_paid = total_paid + extra_payment
+        else:
+            principal = principal * (1+rate/12) - payment
+            total_paid = total_paid + payment
+    count += 1
+
+    print(f'{count} {round(total_paid,2)} {round(principal,2)}')
+
+print('Total paid', total_paid)
+
+print('Months needed', count)
